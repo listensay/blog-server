8ce728e53183583f4b1df96535cde0dbef4d2cdc
听说 <2107228359@qq.com>
2023-09-10 13:23:53 +0800

    完成注册登陆功能

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
+const { expressjwt: jwt } = require('express-jwt')
+const config = require('./config')
 
 // express项目提供的记录日志包
 var cookieParser = require('cookie-parser')
@@ -8,9 +10,18 @@ var cookieParser = require('cookie-parser')
 // express项目提供的记录日志包
 var logger = require('morgan')
 
+var app = express()
+
+// 跨域配置
+const cors = require('cors')
+app.use(cors())
+
+// Token
+app.use(jwt({ secret: config.token_secret, algorithms: ['HS256'] }).unless({ path: ['/api/user/login', '/api/user/register'] }))
+
+// 路由
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/module/users')
-var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -20,7 +31,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', indexRouter)
 
 // 响应信息中间件
 app.use((req, res, next) => {
@@ -41,17 +51,19 @@ app.use((req, res, next) => {
    * @param {*} data 数据 
    * @param {*} success 状态
    */
-  res.success = (data, success = true) => {
+  res.success = (data, message) => {
+    console.log('success')
     res.json({
       code: 2000,
-      data: data,
-      message: 'ok',
-      success: success
+      data,
+      message,
+      success: true
     })
   }
   next()
 })
 
+app.use('/', indexRouter)
 // 用户接口
 app.use('/api/user', usersRouter)
 
@@ -59,6 +71,5 @@ app.use('/api/user', usersRouter)
 app.use(function(req, res, next) {
   next(createError(404))
 })
-
 
 module.exports = app
--- routes/module/users.js
+++ routes/module/users.js
@@ -1,13 +1,51 @@
 const express = require('express')
 const router = express.Router()
 const DB = require('../../db/index')
+const Bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('../../config')
 
 router.post('/login', function (req, res) {
-  DB.query('select * from users', function (err, result) {
+  const { username, password } = req.body
+  // 1.判断是否有用户名
+  DB.query('select * from users where username = ?', username, function (err, result) {
     if (err) return res.error(err)
-    if (result.length > 0) {
-      res.success(result)
+    // 2.用户名不存在 退出
+    if (result.length === 0) return res.error('用户名或密码错误')
+
+    // 3.用户名存在 进行密码匹配
+    const isPasswordValid = Bcrypt.compareSync(password, result[0].password)
+
+    if (isPasswordValid) {
+      const token = jwt.sign({ username }, config.token_secret, { expiresIn: '2h' })
+      console.log(123)
+      res.success(token, '登陆成功')
+    } else {
+      res.error('用户名或密码错误')
     }
+  })
+})
+
+// 注册用户
+router.post('/register', (req, res) => {
+  let { username, password, email } = req.body
+  // 1.首先判断注册的用户名是否存在
+  DB.query('select * from users where username = ?', username, (err, result) => {
+    if (err) return res.error(err)
+    if (result.length > 0) return res.error('用户名已存在')
+    // 用户名不存在, 检查邮箱是否存在
+    DB.query('select * from users where email = ?', email, (err, result) => {
+      if (err) return res.error(err)
+      if (result.length > 0) return res.error('邮箱已存在')
+      // 2.用户名, 邮箱都不存在 可以注册
+      // 3.将密码进行加密
+      password = Bcrypt.hashSync(password, 10)
+      DB.query('INSERT INTO users set ?', { username, password, email }, (err, result) => {
+        if (err) return res.error(err)
+        if (result.affectedRows !== 1) return res.error('注册失败')
+        res.success(null, '注册成功!')
+      })
+    })
   })
 })
 
