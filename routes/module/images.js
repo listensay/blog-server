73bebd7db17bc840c88ce1bdf9759165768c81a9
codemmk <2107228359@qq.com>
2023-10-14 17:02:40 +0800

    图片管理接口编写

--- routes/module/images.js
+++ routes/module/images.js
@@ -15,19 +15,39 @@ router.post('/images', singleMidle, (req, res) => {
   const fileFromat = '.' + file.mimetype.split('/')[1]
   //文件改名保存
   fs.renameSync(PATH + file.filename, PATH + file.filename + fileFromat)
-  res.json({ code: 200, data: { img: '/images/' + file.filename + fileFromat }, message: 'ok', success: true })
+  res.success({ img: '/images/' + file.filename + fileFromat })
 })
 
 router.get('/images', (req, res) => {
   fs.readdir(PATH, (err, files) => {
     if (err) {
       console.error(err)
-      res.status(500).json({ error: 'Server error' })
+      res.error('Server error')
       return
     }
-    console.log(files)
-    const images = files.map(file => '/assets/images/' + file) // 将图片链接的路径更新为正确的路径
-    res.json({ code: 200, data: images, message: 'ok', success: true })
+    const images = []
+    files.forEach(file => {
+      const stat = fs.statSync(PATH + file)
+      const uploadTime = stat.birthtime.toISOString() // 获取文件的创建时间并转换成ISO格式
+      images.push({ url: '/images/' + file, filename: file, uploadTime: uploadTime })
+    })
+    res.success({ list: images })
+  })
+})
+
+router.delete('/images/:filename', (req, res) => {
+  const filename = req.params.filename // 获取要删除的文件名
+  const filePath = PATH + filename // 拼接文件路径
+
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      // 如果删除出错，返回错误信息
+      res.error('Server error')
+      return
+    }
+
+    // 删除成功时，返回成功消息
+    res.success(null, 'Image deleted successfully')
   })
 })
 
